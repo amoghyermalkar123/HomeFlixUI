cde95f1567a68566ef6fe0db746b532aef1ac511
platinumpizza29 <kbelgi1@gmail.com>
2021-05-10 23:37:26 +0530

    createRoom

---
+++ src/pages/homePage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import "./homePage.css";
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <div className="header">
+          <div className="row">
+            <div className="col">1</div>
+            <div className="col">2</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
