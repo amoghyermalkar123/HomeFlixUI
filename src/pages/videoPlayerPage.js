cdaf775c80c8e30702204d5238d5b45d698da5fe
platinumpizza29 <kbelgi1@gmail.com>
2021-05-11 17:20:32 +0530

    tabs

--- src/pages/videoPlayerPage.js
+++ src/pages/videoPlayerPage.js
@@ -1,47 +1,82 @@
 import React, { Component } from "react";
 import VideoPlayer from "./VideoPlayer";
 import "./videoPlayerPage.css";
+import axios from "axios";
 
 import SampleImg from "../assets/avatar.png";
 import SampleImg2 from "../assets/avatar2.jpg";
 import SampleImg3 from "../assets/avatar3.jpg";
-import { Card, CardContent, Avatar } from "@material-ui/core";
 
-export class videoPlayerPage extends Component {
-  render() {
-    return (
-      <div>
-        <div className="cardPlayer">
-          <Card style={{ width: "100%", height: "80vh" }}>
-            <CardContent>
-              <div className="row">
-                <div className="col">
-                  <div className="player">
-                    <VideoPlayer />
-                  </div>
-                </div>
-                <div className="col">
-                  <h1>Participants</h1> <hr /> <br></br>
-                  <Card id="usersLobbyCards">
-                    <Avatar alt="Remy Sharp" src={SampleImg} />
-                    <h2>Amogh Yermalkar</h2>
-                  </Card>
-                  <Card id="usersLobbyCards">
-                    <Avatar alt="Remy Sharp" src={SampleImg} />
-                    <h2>Keyur Bilgi</h2>
-                  </Card>
-                  <Card id="usersLobbyCards">
-                    <Avatar alt="Remy Sharp" src={SampleImg} />
-                    <h2>Samarth Dambalkar</h2>
-                  </Card>
+import People from "@material-ui/icons/People";
+import Movie from "@material-ui/icons/Movie";
+import Message from "@material-ui/icons/Message";
+
+import {
+  Card,
+  CardContent,
+  Avatar,
+  AppBar,
+  Paper,
+  Tabs,
+  Tab,
+} from "@material-ui/core";
+
+export default function VideoPlayerPage() {
+  const [value, setValue] = React.useState(0);
+
+  const [friendList, setFriendList] = React.useState([]);
+
+  const handleFriends = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response);
+        setFriendList(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <div className="cardPlayer">
+        <Card style={{ width: "100%", height: "80vh" }}>
+          <CardContent>
+            <div className="row">
+              <div className="col">
+                <div className="player">
+                  <VideoPlayer />
                 </div>
               </div>
-            </CardContent>
-          </Card>
-        </div>
+              <div className="col">
+                <Paper square>
+                  <Tabs
+                    value={value}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    onChange={(e) => handleChange(e)}
+                  >
+                    <Tab label="Friends" icon={<People />}>
+                      <div className="friends"></div>
+                    </Tab>
+                    <Tab label="Movie Info" icon={<Movie />}>
+                      <div className="movieReview"></div>
+                    </Tab>
+                    <Tab label="Messages(Coming Soon)" icon={<Message />}>
+                      <div className="messages"></div>
+                    </Tab>
+                  </Tabs>
+                </Paper>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default videoPlayerPage;
